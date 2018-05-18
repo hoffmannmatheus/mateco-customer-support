003e3627c04674796574eaa1718d23689e532fdc
Matt Silva <matt.silva@kiswe.com>
2018-05-17 22:47:29 -0400

    Support page is tracking presence, sending/receiving messages

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -10,7 +10,9 @@ const app = new Vue({
   data: {
     currentClient: null,
     clients: [],
+    clientsPresence: {},
     messages: [],
+    ownMessage: "",
     errorMessage: null
   },
 
@@ -23,68 +25,113 @@ const app = new Vue({
     /**
      * Sets up the PubNub client and subscribes to the support channel.
      */
-    setupPunNub: function() {
-      var instance = this;
+    setupPunNub: function () {
+      let instance = this;
 
-      this.pubnub = PUBNUB.init({
-        subscribe_key: 'sub-c-ef77311c-57d7-11e8-b48d-6233128f1163',
-        publish_key: 'pub-c-de29294d-b8f8-449b-94c6-84940ed06a97',
+      this.pubnub = new PubNub({
+        subscribeKey: 'sub-c-ef77311c-57d7-11e8-b48d-6233128f1163',
+        publishKey: 'pub-c-de29294d-b8f8-449b-94c6-84940ed06a97',
         uuid: 'CustomerSupport',
-        ssl: true,
+        presenceTimeout: 20,
         error: function (error) {
           instance.errorMessage = JSON.stringify(error);
         }
       });
 
-      this.pubnub.channel_group_list_channels({
-        channel_group: "customer-support",
-        callback : function(response, error){
-          if (error) {
-            instance.errorMessage = JSON.stringify(error);
+      this.pubnub.channelGroups.listChannels(
+        { channelGroup: "customer-support" },
+        function(status, response){
+          if (status.error) {
+            instance.errorMessage = JSON.stringify(status);
           }
           if (response && response.channels) {
             instance.clients = response.channels;
+            instance.pollClientsPresence();
           }
-        },
-        error : function (error) {
-          console.log(JSON.stringify(error));
         }
-      });
+      );
 
       this.pubnub.addListener({
-        status: function(statusEvent) {
+        status: function (statusEvent) {
           console.log('PubNub status:', statusEvent.category);
         },
-        message: function(message) {
-          instance.handleNewMessage(message);
+        message: function (message) {
+          instance.handleNewMessage(message.message);
         },
-        presence: function(presenceEvent) {
-          // handle presence
-        }
-      })
+        presence: function (presenceEvent) { }
+      });
+    },
+
+    /**
+     * Handle a new incoming message.
+     * @param message The message received.
+     */
+    handleNewMessage: function (message) {
+      if (message && message.type) {
+        this.messages.push(message);
+      }
     },
 
     /**
      * Sends a new message to clients.
-     * @param text The text message..
+     * @param text The text message.
      */
-    sendMessage: function (text) {
-      if (!this.hasClientSelected()) {
+    sendMessage: function () {
+      if (!this.hasClientSelected() || !this.ownMessage) {
         return;
       }
-      // TODO
+      let message = {text: this.ownMessage, type: 'text', sender: 'CustomerService'}
+      pubnub.publish(
+        { message, channel: this.currentClient }, 
+        function(status, response) {
+          if (status.error) {
+            console.log("publishing failed w/ status: ", status);
+          }
+        }
+      );
+      this.ownMessage = "";
     },
 
     /**
-     * Sets the new page.
-     * * @param event The click event.
+     * Sets the new current client.
+     * @param client The client to set to.
      */
     setClient: function (client) {
       if (!client || this.currentClient === client) {
         return;
       }
-      this.currentClient = client;
+      if (this.currentClient) {
+        this.disconnectClient(this.currentClient);
+      }
       this.messages = [];
+      this.currentClient = client;
+      this.connectClient(this.currentClient);
+    },
+
+    /**
+     * Connects from a clent.
+     * @param client The client to connect.
+     */
+    connectClient: function (client) {
+      if (!client) {
+        return;
+      }
+      this.pubnub.subscribe({ 
+        channels: [client] 
+      });
+    },
+
+    /**
+     * Disconnects from a clent.
+     * @param client The client to disconnect.
+     */
+    disconnectClient: function (client) {
+      if (!client) {
+        return;
+      }
+      this.pubnub.unsubscribe({
+        channels: [client]
+      });
     },
 
     /**
@@ -92,8 +139,17 @@ const app = new Vue({
      * @param client The client to check.
      * @returns {boolean} Is this the current client?
      */
-    isClient: function (client) {
+    isCurrentClient: function (client) {
       return this.currentClient === client;
+    },
+
+    /**
+     * Checks if the given client is online.
+     * @param client The client to check.
+     * @returns {boolean} Is this client online?
+     */
+    isClientOnline: function (client) {
+      return !!this.clientsPresence[client];
     },
 
     /**
@@ -102,6 +158,39 @@ const app = new Vue({
      */
     hasClientSelected: function (client) {
       return !!this.currentClient;
+    },
+
+    /**
+     * Periodically checks occupancy on each client channel, to verify presence.
+     */
+    pollClientsPresence: function () {
+      let instance = this;
+
+      let channelHasClient = function(occupants) {
+        // just filter 'me' out
+        return Array.isArray(occupants)
+          && occupants.filter( o => o.uuid != 'CustomerSupport').length > 0;
+      };
+
+      let poll = function () {
+        instance.pubnub.hereNow(
+          { channels: this.clients, includeState: true },
+          function (status, response) {
+            if (status.error) {
+              return;
+            }
+            let newPresenceState = {};
+            for (let channel in response.channels) {
+              newPresenceState[channel] = channelHasClient(response.channels[channel].occupants);
+            }
+            instance.clientsPresence = newPresenceState;
+          }
+        );
+      };
+      // infinitely poll
+      setInterval(poll, 2000);
+      // and once immediately
+      poll();
     }
   }
 });
